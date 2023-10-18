259d04d3e21ebcf1d6edfbf2c70977a1e0e3e1f6
Rifa9oct <kohinuraktherr@gmail.com>
2023-10-18 14:32:26 +0600

    added navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,9 +1,45 @@
-
+import { Link, NavLink } from "react-router-dom";
+import logo from "../assets/logo.png"
+import { BsDatabaseFillAdd } from "react-icons/bs";
+import { AiFillHome } from "react-icons/ai";
+import { FaCartShopping } from "react-icons/fa6";
 
 const Navbar = () => {
+
+    const navLinks = <>
+        <li> <NavLink to="/" className={({ isActive, isPending }) =>
+            isActive ? "active rounded-lg  text-red-600" : isPending ? "pending" : ""}><AiFillHome className="mx-auto text-2xl"></AiFillHome>Home</NavLink>
+        </li>
+        <li> <NavLink to="/addProduct" className={({ isActive, isPending }) =>
+            isActive ? "active rounded-lg  text-red-600" : isPending ? "pending" : ""}><BsDatabaseFillAdd className="mx-auto text-2xl"></BsDatabaseFillAdd>Add Product</NavLink>
+        </li>
+        <li> <NavLink to="/mycart" className={({ isActive, isPending }) =>
+            isActive ? "active rounded-lg  text-red-600" : isPending ? "pending" : ""}><FaCartShopping className="mx-auto text-2xl"></FaCartShopping>My Cart</NavLink>
+        </li>
+    </>
+
     return (
-        <div>
-           
+        <div className="navbar mb-10 flex-col md:flex-row justify-between bg-orange-50">
+            <div>
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                    </label>
+                    <ul tabIndex={0} className="flex flex-col gap-4 dropdown-content mt-3 z-[1] p-6 py-6 shadow bg-base-100 rounded-box text-center text-sm w-52">
+                        {navLinks}
+                    </ul>
+                </div>
+                <img className="h-20" src={logo} />
+                <h1 className="text-2xl font-pacifico">Fashion Brand Shop</h1>
+            </div>
+            <div>
+                <div className="hidden mr-8 lg:flex">
+                    <ul className="flex gap-8">
+                        {navLinks}
+                    </ul>
+                </div>
+                <Link to="/login" className="btn md:mr-4 bg-orange-500 hover:bg-orange-700 text-white">login</Link>
+            </div>
         </div>
     );
 };
--- src/routes/router/router.jsx
+++ src/routes/router/router.jsx
@@ -1,6 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../../Root/Root";
 import Home from "../../Pages/Home/Home";
+import AddProduct from "../../Pages/AddProduct/AddProduct";
+import MyCart from "../../Pages/MyCart/MyCart";
+import Login from "../../Pages/Login/Login";
+
 
 const router = createBrowserRouter([
     {
@@ -10,6 +14,18 @@ const router = createBrowserRouter([
         {
             path:"/",
             element:<Home></Home>
+        },
+        {
+            path:"/addProduct",
+            element:<AddProduct></AddProduct>
+        },
+        {
+            path:"/mycart",
+            element:<MyCart></MyCart>
+        },
+        {
+            path:"/login",
+            element:<Login></Login>
         },
       ]
     },
