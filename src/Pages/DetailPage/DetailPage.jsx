9c83d18d973b63d0d2290ffd479700f21cfed37f
Rifa9oct <kohinuraktherr@gmail.com>
2023-10-20 14:41:12 +0600

    stored cart data in the database

--- src/Pages/DetailPage/DetailPage.jsx
+++ src/Pages/DetailPage/DetailPage.jsx
@@ -1,23 +1,49 @@
 import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const DetailPage = () => {
     const loadedProduct = useLoaderData();
     console.log(loadedProduct);
-    const { name, photoUrl, description } = loadedProduct;
+    const { photoUrl, name,description } = loadedProduct;
 
-    return (
-        <div className="max-w-[1320px] mx-auto">
-            <h1 className="text-4xl text-center mt-10 text-blue-600">Product Detail</h1>
-            <div className="flex flex-col-reverse  gap-8 md:flex-row md:my-20 justify-center items-center p-5">
-                <div className="md:w-2/4">
-                    <h1 className="underline-offset-4 mt-5 md:mt-0 underline mb-5 text-2xl font-bold">{name} :</h1>
-                    <p className="text-gray-500">{description}</p>
-                    <button className="btn text-sm bg-blue-400 mt-5 text-white hover:bg-blue-600">Add to Cart</button>
-                </div>
-                <img className="md:w-1/4" src={photoUrl} />
+    const mycart = loadedProduct;
+    console.log(mycart)
+
+    const handleClick = () => {
+        fetch('https://fashion-brand-shop-server-side.vercel.app/mycarts', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(mycart)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Good job!',
+                        text: 'Cart added successfully.',
+                        icon: 'success',
+                        confirmButtonText: 'OK'
+                    })
+                }
+            })
+    }
+
+return (
+    <div className="max-w-[1320px] mx-auto">
+        <h1 className="text-4xl text-center mt-10 text-blue-600">Product Detail</h1>
+        <div className="flex flex-col-reverse  gap-8 md:flex-row md:my-20 justify-center items-center p-5">
+            <div className="md:w-2/4">
+                <h1 className="underline-offset-4 mt-5 md:mt-0 underline mb-5 text-2xl font-bold">{name} :</h1>
+                <p className="text-gray-500">{description}</p>
+                <button onClick={handleClick} className="btn text-sm bg-blue-400 mt-5 text-white hover:bg-blue-600">Add to Cart</button>
             </div>
+            <img className="md:w-1/4" src={photoUrl} />
         </div>
-    );
+    </div>
+);
 };
 
 export default DetailPage;
